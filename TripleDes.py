f1c00aaeecba17fb8c883d5c69ef986f4d299132
Besarta Kurtaj <93934264+besartakurtaj@users.noreply.github.com>
2022-05-15 17:29:07 +0200

    TripleDes update

--- TripleDes.py
+++ TripleDes.py
@@ -15,3 +15,12 @@ key_hash = md5(key.encode('ascii')).digest()
 
     tdes_key = DES3.adjust_key_parity(key_hash)
     cipher = DES3.new(tdes_key, DES3.MODE_EAX, nonce = b'0')
+    
+    
+    
+
+    
+    with open(file_path, 'wb') as output_file:
+        output_file.write(new_file_bytes)
+        print("Operation DONE!")
+        break
