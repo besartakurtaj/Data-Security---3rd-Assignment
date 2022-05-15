97fdbaeb479db50a89173beae146b6a00bede549
Besarta Kurtaj <93934264+besartakurtaj@users.noreply.github.com>
2022-05-15 16:02:03 +0200

    TripleDes

--- TripleDes.py
+++ TripleDes.py
@@ -10,3 +10,8 @@ while True:
         break
     file_path = input('File path: ')
     key = input('TDES key: ')
+    
+key_hash = md5(key.encode('ascii')).digest()
+
+    tdes_key = DES3.adjust_key_parity(key_hash)
+    cipher = DES3.new(tdes_key, DES3.MODE_EAX, nonce = b'0')
