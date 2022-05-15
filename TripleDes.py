6d7d8d593e1f24bee7f70bbcfe2fc25d7846a765
Aulona Limani <94397920+AulonaLimani@users.noreply.github.com>
2022-05-15 18:01:57 +0200

    Update TripleDes.py

--- TripleDes.py
+++ TripleDes.py
@@ -17,8 +17,13 @@ key_hash = md5(key.encode('ascii')).digest()
     cipher = DES3.new(tdes_key, DES3.MODE_EAX, nonce = b'0')
     
     
-    
+    with open(file_path, 'rb') as input_file:
+        file_bytes = input_file.read()
 
+
+        if operation == '1':
+            new_file_bytes = cipher.encrypt(file_bytes)
+       
     
     with open(file_path, 'wb') as output_file:
         output_file.write(new_file_bytes)
