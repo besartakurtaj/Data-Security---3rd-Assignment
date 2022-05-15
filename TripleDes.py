24342cf64d0a58f7b8dd0981c253ede4170fe89a
Dredhza Braina <94399125+DredhzaBraina@users.noreply.github.com>
2022-05-15 15:44:59 +0200

    Update TripleDes.py

--- TripleDes.py
+++ TripleDes.py
@@ -2,3 +2,6 @@ import Crypto
 from ast import operator
 from Crypto.Cipher import DES3
 from hashlib import md5
+
+while True:
+    print('Choose one of the following operations: \n\t 1- Encrypt\n\t2 - Decrypt')
